5ab88be28059ab7bd00fadccbb4ba3cb3a19a135
Jon Mischo <jmischo@quagility.com>
2010-05-08 00:19:53 -0500

    Implemented rudimentary environment management

--- app/models/environment.rb
+++ app/models/environment.rb
@@ -1,3 +1,8 @@
 class Environment < ActiveRecord::Base
   attr_accessible :name, :enable_at, :disable_at, :restricted_to, :description
+
+  validates_presence_of :name
+  validates_length_of :name, :minimum => 2
+  validates_presence_of :description
+  validates_length_of :description, :minimum => 20
 end
