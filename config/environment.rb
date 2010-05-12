783cc2d5ed0edfacd0b809038c166f114672cae9
Jon Mischo <jmischo@quagility.com>
2010-05-12 11:16:02 -0500

    Added some missing gem requirements

--- config/environment.rb
+++ config/environment.rb
@@ -31,7 +31,8 @@ Rails::Initializer.run do |config|
   config.gem 'validation_reflection'
   config.gem 'pivotal-tracker'
   config.gem 'authlogic'
-
+  config.gem 'compass'
+  config.gem 'searchlogic'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
