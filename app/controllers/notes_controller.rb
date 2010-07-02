34ad22e690d282463945adedae1dcbf14d97231a
Jon Mischo <jmischo@quagility.com>
2010-07-02 16:05:04 -0500

    Changed a little code to avoid double posted notes and lost notes when pivotal fails...

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -5,15 +5,21 @@ class NotesController < ApplicationController
     note.story = story
     note.author = current_user
     unless story.story_source.nil? || story.story_source != 'pivotal'
-      pivotal_project = story.project.get_source_project
-      pivotal_story = pivotal_project.stories.find(story.source_id)
-      if note.subject.empty?
-        note_header = "*#{current_user.fullname} said:*\n"
-      else
-        note_header = "*#{current_user.fullname} talked about \"#{note.subject}\":*\n"
+      begin
+        pivotal_project = story.project.get_source_project
+        pivotal_story = pivotal_project.stories.find(story.source_id)
+        if note.subject.empty?
+          note_header = "*#{current_user.fullname} said:*\n"
+        else
+          note_header = "*#{current_user.fullname} talked about \"#{note.subject}\":*\n"
+        end
+        pivotal_story.notes.create(:text => note_header + note.body)
+      rescue
+        flash.now[:error] = 'Unable to create note, please try again. If this continues, please contact your administrator.'
+        @story = story
+        @note = note
+        render 'stories/show'
       end
-      pivotal_story.notes.create(:text => note_header + note.body)
-      note.save
     else
       note.save
     end
