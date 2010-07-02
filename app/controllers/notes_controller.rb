c7d67ea8744997147945c96037aa6f7c62864624
Jon Mischo <jmischo@quagility.com>
2010-07-02 16:40:53 -0500

    Verbose exception logging...

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -16,6 +16,7 @@ class NotesController < ApplicationController
         pivotal_story.notes.create(:text => note_header + note.body)
       rescue => e
         error_message = 'Unable to create note, please try again. If this continues, please contact your administrator.<br>'
+        logger.error e.backtrace
         if e.respond_to? 'response'
           if e.response.nil?
             error_message += "#{e.class} exception#{e.respond_to?('message') ? ':' + e.message : '.'}"
